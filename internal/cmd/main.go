df3073ee50c081c87ab219e2f7613cefc51450f7
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-25 23:07:14 +0330

    cmd running added

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,1 +1,32 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
+
+func serverCmd() *cobra.Command {
+	return &cobra.Command{
+		Use: "server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// Handle Subsequent requests
+			// handleRequests()
+			return nil
+		},
+	}
+}
+
+func Run() {
+	cmd := &cobra.Command{
+		Use:     "dispatch",
+		Short:   "Dispatch Server",
+		Version: "0.1",
+	}
+
+	cmd.AddCommand(serverCmd())
+
+	if err := cmd.Execute(); err != nil {
+		//fmt.Println(err)
+		os.Exit(0)
+	}
+}
