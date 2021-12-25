5b3d39db243c52a4cc82bad5a9ad24a6f8b578ff
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-25 23:40:52 +0330

    server init

--- config/main.go
+++ config/main.go
@@ -38,3 +38,7 @@ func LoadConfiguration(file string) Config {
 
 	return config
 }
+
+func GetConfig() Config {
+	return config
+}
--- server/main.go
+++ server/main.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+	"cmd/config"
+	"fmt"
+	"log"
+)
+
+func Exec() {
+	config := config.GetConfig()
+
+	// Launch HTTP server
+	go func() {
+		fmt.Println("Starting server http://localhost")
+
+		err := httpSrv.ListenAndServe()
+		if err != nil {
+			log.Fatalf("httpSrv.ListenAndServe() failed with %s", err)
+		}
+
+	}()
+
+	// Launch HTTPS server
+	fmt.Println("Starting server https://" + config.Host + ":" + config.Port)
+	log.Fatal(httpsSrv.ListenAndServeTLS("", ""))
+}
