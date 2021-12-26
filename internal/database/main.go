889b4f496d40ba4d77021bf5240ece6ee965cc67
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-26 11:56:44 +0330

    gorm database config and setup added

--- internal/database/main.go
+++ internal/database/main.go
@@ -1,1 +1,29 @@
 package database
+
+import (
+	"cmd/internal/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func initialMigration() {
+	// Migrate the project schema
+	_ = db.AutoMigrate(&models.Task{})
+}
+
+func Setup(migrate bool) *gorm.DB {
+	var err error
+	db, err = gorm.Open(sqlite.Open("sql.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("database connection: faild")
+	}
+
+	if migrate {
+		initialMigration()
+	}
+
+	return db
+}
