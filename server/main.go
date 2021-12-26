ba2103a02c097c127ae35b000bbb2f37c32c5e9d
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-26 11:43:28 +0330

    server main file bugs commented for now

--- server/main.go
+++ server/main.go
@@ -3,24 +3,23 @@ package server
 import (
 	"cmd/config"
 	"fmt"
-	"log"
 )
 
 func Exec() {
-	config := config.GetConfig()
+	configuration := config.GetConfig()
 
 	// Launch HTTP server
 	go func() {
 		fmt.Println("Starting server http://localhost")
 
-		err := httpSrv.ListenAndServe()
-		if err != nil {
-			log.Fatalf("httpSrv.ListenAndServe() failed with %s", err)
-		}
+		//err := httpSrv.ListenAndServe()
+		//if err != nil {
+		//	log.Fatalf("httpSrv.ListenAndServe() failed with %s", err)
+		//}
 
 	}()
 
 	// Launch HTTPS server
-	fmt.Println("Starting server https://" + config.Host + ":" + config.Port)
-	log.Fatal(httpsSrv.ListenAndServeTLS("", ""))
+	fmt.Println("Starting server https://" + configuration.Host + ":" + configuration.Port)
+	// log.Fatal(httpsSrv.ListenAndServeTLS("", ""))
 }
