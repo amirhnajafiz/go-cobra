877b859d5cef777deddae74455abba9830270ba1
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-26 19:45:47 +0330

    server dispatch got divided

--- internal/command/dispatch.go
+++ internal/command/dispatch.go
@@ -6,39 +6,37 @@ import (
 	"gorm.io/gorm"
 )
 
+var message = `[33mUsage:[0m{{if .Runnable}}
+{{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+{{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+Aliases:
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+[33mAvailable Commands:[0m {{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+[33mFlags:[0m 
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}`
+
 func InitCommands(configuration config.Config, db *gorm.DB) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "dispatch",
 		Short:   "Dispatch Server",
 		Version: "0.1",
 	}
-
-	cmd.SetUsageTemplate(`[33mUsage:[0m{{if .Runnable}}
-		  	{{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
-		  	{{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
-		
-			Aliases:
-			  {{.NameAndAliases}}{{end}}{{if .HasExample}}
-			
-			Examples:
-			{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
-			
-			[33mAvailable Commands:[0m {{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
-			  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
-			
-			[33mFlags:[0m 
-			{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
-			
-			Global Flags:
-			{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
-			
-			Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
-			  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
-			
-			Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
-	`)
-
+	cmd.SetUsageTemplate(message)
 	cmd.AddCommand(ServerCmd(configuration, db))
-
 	return cmd
 }
--- makefile
+++ makefile
@@ -1,7 +1,12 @@
 # Web-cli builder
 # web-cli is a command line, dispatcher api, web application where you
 # can communicate with a server and execute your commands.
-dev:
+serve:
 	clear
 	echo "Building Application ..."
 	go run cmd/main.go server
+
+dispatch:
+	clear
+	echo "Importing Usages ..."
+	go run cmd/main.go
