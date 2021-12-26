06316e6d5d13723b9847251f110ff4adb18fdb31
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-26 14:44:58 +0330

    cmd usage set

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -23,11 +23,37 @@ func serverCmd(configuration config.Config, db *gorm.DB) *cobra.Command {
 func Exec() {
 	db := database.Setup(true)
 	configuration := config.GetConfig()
+
 	cmd := &cobra.Command{
 		Use:     "dispatch",
 		Short:   "Dispatch Server",
 		Version: "0.1",
 	}
+
+	cmd.SetUsageTemplate(`[33mUsage:[0m{{if .Runnable}}
+		  	{{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+		  	{{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+		
+			Aliases:
+			  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+			
+			Examples:
+			{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+			
+			[33mAvailable Commands:[0m {{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+			  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+			
+			[33mFlags:[0m 
+			{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+			
+			Global Flags:
+			{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+			
+			Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+			  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+			
+			Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+	`)
 
 	cmd.AddCommand(serverCmd(configuration, db))
 
