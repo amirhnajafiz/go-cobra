4f4030f0c355812fb9df5a1359f50b7d69ec0057
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-10 20:32:15 +0330

    config of database added

--- internal/config/type.go
+++ internal/config/type.go
@@ -2,8 +2,10 @@ package config
 
 // Config Storing all the configurations inside this variable
 type Config struct {
-	Token   string `json:"token"`
-	Host    string `json:"host"`
-	Port    string `json:"port"`
-	SSLMode string `json:"ssl_mode"`
+	Token     string `json:"token"`
+	Host      string `json:"host"`
+	Port      string `json:"port"`
+	SSLMode   string `json:"ssl_mode"`
+	Database  string `json:"database"`
+	Migration bool   `json:"migration"`
 }
