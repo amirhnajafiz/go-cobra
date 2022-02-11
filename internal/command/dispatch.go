967e67c8af80bd5e4fb8fbf586dd87abead9cd9c
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-11 19:04:51 +0330

    commander refactor

--- internal/command/dispatch.go
+++ internal/command/dispatch.go
@@ -2,41 +2,10 @@ package command
 
 import (
 	"cmd/internal/config"
-	"github.com/spf13/cobra"
 	"gorm.io/gorm"
 )
 
-var message = `[33mUsage:[0m{{if .Runnable}}
-{{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
-{{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
-
-Aliases:
-  {{.NameAndAliases}}{{end}}{{if .HasExample}}
-
-Examples:
-{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
-
-[33mAvailable Commands:[0m {{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
-  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
-
-[33mFlags:[0m 
-{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
-
-Global Flags:
-{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
-
-Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
-  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
-
-Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}`
-
-func InitCommands(configuration config.Config, db *gorm.DB) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:     "dispatch",
-		Short:   "Dispatch Server",
-		Version: "0.1",
-	}
-	cmd.SetUsageTemplate(message)
-	cmd.AddCommand(ServerCmd(configuration, db))
-	return cmd
+type Commander struct {
+	Configuration config.Config
+	DB            *gorm.DB
 }
--- internal/command/server.go
+++ internal/command/server.go
@@ -2,15 +2,48 @@ package command
 
 import (
 	"cmd/internal/cmd/server"
-	"cmd/internal/config"
 	"github.com/spf13/cobra"
-	"gorm.io/gorm"
 )
 
-func ServerCmd(configuration config.Config, db *gorm.DB) *cobra.Command {
+var message = `[33mUsage:[0m{{if .Runnable}}
+{{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+{{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+Aliases:
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+[33mAvailable Commands:[0m {{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+[33mFlags:[0m 
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}`
+
+func (c Commander) InitCommands() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "dispatch",
+		Short:   "Dispatch Server",
+		Version: "0.1",
+	}
+	cmd.SetUsageTemplate(message)
+	cmd.AddCommand(c.ServerCmd())
+	return cmd
+}
+
+func (c Commander) ServerCmd() *cobra.Command {
 	set := server.Setup{
-		Configuration: configuration,
-		DB:            db,
+		Configuration: c.Configuration,
+		DB:            c.DB,
 	}
 	return &cobra.Command{
 		Use: "server",
