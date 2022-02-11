e09fb273e02ab7c623b4d7789d0855fee33b5235
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-11 18:15:58 +0330

    auth middleware bug fixed

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -7,10 +7,14 @@ import (
 
 type HttpHandlerFunc func(http.ResponseWriter, *http.Request)
 
-func Auth(configuration config.Config, next HttpHandlerFunc) HttpHandlerFunc {
+type Middleware struct {
+	Configuration config.Config
+}
+
+func (m Middleware) Auth(next HttpHandlerFunc) HttpHandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		header := r.Header.Get("token")
-		if header != configuration.Token {
+		if header != m.Configuration.Token {
 			w.WriteHeader(http.StatusUnauthorized)
 			_, _ = w.Write([]byte("401 - Unauthorized"))
 			return
