4ac19974e2de2ed1ec3800e569f084fcdacc9a46
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-24 15:08:04 +0430

    add: crypto cobra

--- internal/cmd/crypto/crypto.go
+++ internal/cmd/crypto/crypto.go
@@ -3,7 +3,19 @@ package crypto
 import (
 	"github.com/amirhnajafiz/go-cobra/pkg/encrypt"
 	"github.com/amirhnajafiz/go-cobra/pkg/logger"
+	"github.com/spf13/cobra"
 )
+
+func GetCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "crypto",
+		Short: "generate certifications",
+		Long:  "command for creating certification files in certs directory",
+		Run: func(_ *cobra.Command, _ []string) {
+			main()
+		},
+	}
+}
 
 func main() {
 	l := logger.New()
