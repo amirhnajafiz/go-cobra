22ddfc4766d506e785cce4d10a179e4e2b2e3aab
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-24 12:05:17 +0430

    update: config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,31 +2,38 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 )
 
-var file = "config.json"
+const (
+	configFileName = "config.json"
+)
+
+// Config Storing all the configurations inside this variable
+type Config struct {
+	Token     string `json:"token"`
+	Host      string `json:"host"`
+	Port      string `json:"port"`
+	SSLMode   string `json:"ssl_mode"`
+	Database  string `json:"database"`
+	Migration bool   `json:"migration"`
+}
 
 // LoadConfiguration method opens the 'config.json' file
 // and imports the server configs into the config variable.
-func LoadConfiguration() Config {
+func LoadConfiguration() (*Config, error) {
 	var config Config
 
-	configFile, err := os.Open(file)
-	defer func(configFile *os.File) {
-		err := configFile.Close()
-		if err != nil {
-			panic(err.Error())
-		}
-	}(configFile)
-
+	configFile, err := os.Open(configFileName)
 	if err != nil {
-		fmt.Println(err.Error())
+		return nil, err
 	}
 
-	jsonParser := json.NewDecoder(configFile)
-	_ = jsonParser.Decode(&config)
+	defer configFile.Close()
 
-	return config
+	if er := json.NewDecoder(configFile).Decode(&config); er != nil {
+		return nil, er
+	}
+
+	return &config, nil
 }
