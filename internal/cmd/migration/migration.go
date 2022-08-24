12c7c1da616e30f913db804790f4102e882c3d77
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-24 12:25:02 +0430

    add: migration command

--- internal/cmd/migration/migration.go
+++ internal/cmd/migration/migration.go
@@ -1,1 +1,15 @@
 package migration
+
+import (
+	"github.com/amirhnajafiz/go-cobra/internal/database"
+	"github.com/amirhnajafiz/go-cobra/pkg/logger"
+	"go.uber.org/zap"
+)
+
+func main() {
+	l := logger.New()
+
+	if err := database.Migrate(); err != nil {
+		l.Error("migration failed", zap.Error(err))
+	}
+}
