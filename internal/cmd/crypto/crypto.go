312e1b1c6a227bf37609236963b72220a80ce9a5
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-24 12:23:49 +0430

    add: crypto command

--- internal/cmd/crypto/crypto.go
+++ internal/cmd/crypto/crypto.go
@@ -1,1 +1,17 @@
 package crypto
+
+import (
+	"github.com/amirhnajafiz/go-cobra/pkg/encrypt"
+	"github.com/amirhnajafiz/go-cobra/pkg/logger"
+)
+
+func main() {
+	l := logger.New()
+
+	enc := encrypt.Encrypt{
+		Logger: l.Named("crypto"),
+	}
+
+	enc.GenerateCertificateAuthority()
+	enc.GenerateCert()
+}
