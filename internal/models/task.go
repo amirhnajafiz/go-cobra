0efb3640ee41c5d6db557ec100ab8a8f23f249b2
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-24 11:51:10 +0430

    fix: models

--- internal/models/task.go
+++ internal/models/task.go
@@ -1,21 +1,35 @@
 package models
 
 import (
-	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation"
 	"gorm.io/gorm"
 )
 
 type Task struct {
 	gorm.Model
+
 	Command  string `json:"command"`
 	Status   string `json:"status"`
 	Response string `json:"response"`
 }
 
 func (t Task) Validate() error {
-	return validation.ValidateStruct(&t,
-		validation.Field(&t.Command, validation.Required, validation.Length(0, 255)),
-		validation.Field(&t.Status, validation.Required, validation.Length(0, 255)),
-		validation.Field(&t.Response, validation.Required, validation.Length(0, 255)),
+	return validation.ValidateStruct(
+		&t,
+		validation.Field(
+			&t.Command,
+			validation.Required,
+			validation.Length(0, 255),
+		),
+		validation.Field(
+			&t.Status,
+			validation.Required,
+			validation.Length(0, 255),
+		),
+		validation.Field(
+			&t.Response,
+			validation.Required,
+			validation.Length(0, 255),
+		),
 	)
 }
