ef95c09b2407ac7347e45a6f2636b460b9411756
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-24 15:09:35 +0430

    add: migration cobra

--- internal/cmd/migration/migration.go
+++ internal/cmd/migration/migration.go
@@ -3,13 +3,29 @@ package migration
 import (
 	"github.com/amirhnajafiz/go-cobra/internal/database"
 	"github.com/amirhnajafiz/go-cobra/pkg/logger"
+	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
+
+func GetCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "migrate",
+		Short: "migrate",
+		Long:  "migrate database (sqlite3)",
+		Run: func(_ *cobra.Command, _ []string) {
+			main()
+		},
+	}
+}
 
 func main() {
 	l := logger.New()
 
 	if err := database.Migrate(); err != nil {
 		l.Error("migration failed", zap.Error(err))
+
+		return
 	}
+
+	l.Info("migrate successfully")
 }
